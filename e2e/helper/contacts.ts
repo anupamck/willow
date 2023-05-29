c922f0de9ea1e787dcfffcbd5b6622e800b12ce6
anupamck <anupam.ck@gmailcom>
2023-05-29 17:57:42 +0200

    Adapt test to handle edge cases with quotes and double-quotes

--- e2e/helper/contacts.ts
+++ e2e/helper/contacts.ts
@@ -1,10 +1,10 @@
 export async function deleteTestContactRows(page) {
     const testUserRows = page.getByRole('row')
-        .filter({ hasText: 'Test User' })
+        .filter({ hasText: "Test User's" })
         .filter({ hasText: '20' });
 
     const testUserEditedRows = page.getByRole('row')
-        .filter({ hasText: 'Test User 2' })
+        .filter({ hasText: '"Test User 2"' })
         .filter({ hasText: '30' });
 
     page.on('dialog', dialog => dialog.accept());
@@ -18,7 +18,7 @@ export async function deleteTestContactRows(page) {
 
 export async function createTestContactRow(page) {
     await page.getByRole('button', { name: 'Add Contact' }).click();
-    await page.getByLabel('Name:').fill('Test User');
+    await page.getByLabel('Name:').fill("Test User's");
     await page.getByLabel('Frequency:').fill('20');
     await page.getByRole('button', { name: 'Save Changes' }).click();
 }
--- e2e/tests/contacts.spec.ts
+++ e2e/tests/contacts.spec.ts
@@ -11,27 +11,28 @@ test('contacts page has the correct layout', async ({ page }) => {
     await expect(page).toHaveURL('/contacts');
     await expect(page.getByRole('heading', { name: 'My Contacts' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Home' })).toBeVisible();
-    await expect(page.getByRole('table', {  name: /contacts\-table/i})).toBeVisible();
+    await expect(page.getByRole('table', { name: /contacts\-table/ })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Add Contact' })).toBeVisible();
 });
 
-test('should be able to edit test contact', async ({ page }) => {
-    await page.goto('./contacts');
+test.only('should be able to edit test contact', async ({ page }) => {
+    const testUserName = "Test User's";
+    const testUserNameEdited = '"Test User 2"';
     await createTestContactRow(page);
     const testUserRow = page.getByRole('row')
-        .filter({ hasText: 'Test User' })
+        .filter({ hasText: testUserName })
         .filter({ hasText: '20' });
     await testUserRow.getByRole('button', { name: 'Edit' }).click();
     // give me a regex to ensure that the url contains 'update_contact'
     await expect(page).toHaveURL(/\/update_contact\/.*/)
-    await expect(page.getByLabel('Name:')).toHaveValue('Test User');
+    await expect(page.getByLabel('Name:')).toHaveValue(testUserName);
     await expect(page.getByLabel('Frequency:')).toHaveValue('20');
-    await page.getByLabel('Name:').fill('Test User 2');
+    await page.getByLabel('Name:').fill(testUserNameEdited);
     await page.getByLabel('Frequency:').fill('30');
     await page.getByRole('button', { name: 'Save Changes' }).click();
     await expect(page).toHaveURL('/contacts');
     const editedRow = page.getByRole('row')
-        .filter({ hasText: 'Test User 2' })
+        .filter({ hasText: testUserNameEdited })
         .filter({ hasText: '30' });
     expect(await editedRow.count()).toBe(1);
 });
