2990795c08d8df45e73b59231f7b1ecd68d05e61
anupamck <anupam.ck@gmailcom>
2023-05-29 09:03:58 +0200

    Remove ugly hack to deal with HTML encoding bug

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_contacts():
             # Convert the list of lists to a list of dictionaries
             contact_dicts = []
             for contact in contacts:
-                contact_dict = {"id": contact[0], "name": contact[1].replace("'", "&#39;").replace('"', "&#34;").replace("\n", ""), "frequency": contact[2]}
+                contact_dict = {"id": contact[0], "name": contact[1], "frequency": contact[2]}
                 contact_dicts.append(contact_dict)
         return render_template('contacts.html', contacts=contact_dicts)
 
@@ -62,7 +62,7 @@ def get_home():
             # Convert the list of lists to a list of dictionaries
             overdue_dicts = []
             for contact in overdue_contacts:
-                overdue_dict = {"id": contact[0], "name": contact[1].replace("'", "&#39;").replace('"', "&#34;").replace("\n", ""), "frequency": contact[2], "last_interaction": contact[3]}
+                overdue_dict = {"id": contact[0], "name": contact[1], "frequency": contact[2], "last_interaction": contact[3]}
                 overdue_dicts.append(overdue_dict)
             return render_template('home.html', contacts=overdue_dicts)
         
