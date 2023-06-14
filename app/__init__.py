e7a2950dd7c7109fedeac37c592e59b4d18ab73a
anupamck <anupam.ck@gmailcom>
2023-06-14 08:05:46 +0200

    Add strong secret key

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from .routes.contacts import contacts_bp
 from .routes.interactions import interactions_bp
 from .routes.login import login_bp
 from flask_cors import CORS
+import os
 
 # Create Flask app
 app = Flask(__name__)
 app.config.from_mapping(
-    SECRET_KEY='dev'
+    SECRET_KEY=os.getenv('SECRET_KEY_TEST'),
 )
 app.register_blueprint(home_bp)
 app.register_blueprint(contacts_bp)
