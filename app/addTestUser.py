e91c107537e0e8e433465e8349f2c5aa099d8e0c
anupamck <anupam.ck@gmailcom>
2023-06-14 08:06:27 +0200

    Extend test user addition script

--- app/addTestUser.py
+++ app/addTestUser.py
@@ -11,6 +11,14 @@ config = {
             'database': 'u936540649_willowUsers'
 }
 
+config_user = {
+    'user': 'u936540649_willowTest',
+            'password': os.getenv('TEST_USER_PASSWORD'),
+            'host': 'srv976.hstgr.io',
+            'database': 'u936540649_willowTest'
+}
+
 with DatabaseConnector(config=config) as connector:
     user_manager = UserManager(connector)
-    user_manager.add_user('ashoka', os.getenv('TEST_USER_PASSWORD'))
+    user_manager.add_user('ashoka', os.getenv(
+        'TEST_USER_PASSWORD'), 'ashoka@maghada.com', config_user)
