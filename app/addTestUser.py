5d4eb09434da1d8a658f675a4234c03c3f9e95e3
anupamck <anupam.ck@gmailcom>
2023-06-15 20:22:13 +0200

    Rename

--- app/addTestUser.py
+++ app/addTestUser.py
@@ -4,21 +4,21 @@ import os
 
 load_dotenv()
 
-config = {
+config_users_db = {
     'user': 'u936540649_willowUsers',
             'password': os.getenv('USER_DB_PASSWORD'),
             'host': 'srv976.hstgr.io',
             'database': 'u936540649_willowUsers'
 }
 
-config_user = {
+config_test_user_db = {
     'user': 'u936540649_willowTest',
             'password': os.getenv('TEST_USER_PASSWORD'),
             'host': 'srv976.hstgr.io',
             'database': 'u936540649_willowTest'
 }
 
-with DatabaseConnector(config=config) as connector:
+with DatabaseConnector(config=config_users_db) as connector:
     user_manager = UserManager(connector)
     user_manager.add_user('ashoka', os.getenv(
-        'TEST_USER_PASSWORD'), 'ashoka@maghada.com', config_user)
+        'TEST_USER_PASSWORD'), 'ashoka@maghada.com', config_test_user_db)
