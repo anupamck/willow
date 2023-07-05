ed61e49c9e5761240491f3ecee30e208cb42c4d7
anupamck <anupam.ck@gmailcom>
2023-07-05 21:49:01 +0200

    Combine scripts to add new user

--- app/addUser.py
+++ app/addUser.py
@@ -1,15 +1,23 @@
 from routes.db import DatabaseConnector, UserManager, ContactManager
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 load_dotenv()
 
-username = 'newTestUser'
+username = 'kalaikovan'
 password = os.getenv('NEW_USER_PASSWORD')
-email = 'newuser@gmail.com'
+email = 'kalai.march90@gmail.com'
 
 users_db = os.path.join(os.getenv('DB_PATH'), "users.db")
+db_path = os.path.join(os.getenv('DB_PATH'), username + '.db')
 
 with DatabaseConnector(database=users_db) as connector:
     user_manager = UserManager(connector)
     user_manager.add_user(username, password, email, username + ".db")
+
+conn = sqlite3.connect(db_path)
+conn.close()
+with DatabaseConnector(database=db_path) as connector:
+    user_manager = UserManager(connector)
+    user_manager.initialize_user_db()
