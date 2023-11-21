09aec6c3b9d21e237ba09b0469839fd8a6f221d7
anupamck <anupam.ck@gmailcom>
2023-11-21 19:00:29 +0100

    Remove password confirmation from E2E tests

--- e2e/tests/registration.spec.ts
+++ e2e/tests/registration.spec.ts
@@ -8,7 +8,6 @@ test("User cannot register with an existing username", async ({ page }) => {
   await page.getByLabel("Username").type("ashoka");
   await page.getByLabel("E-mail").type("ashoka@email.com");
   await page.getByLabel("Password:", { exact: true }).type("password");
-  await page.getByLabel("Confirm Password").type("password");
   await page.getByRole("button", { name: "Register" }).click();
   await expect(page.getByText("Username is already taken")).toBeVisible();
 });
@@ -19,20 +18,8 @@ test("User cannot register with an existing email address", async ({
   await page.getByLabel("Username").type("ashoka1");
   await page.getByLabel("E-mail").type("ashoka@maghada.com");
   await page.getByLabel("Password:", { exact: true }).type("password");
-  await page.getByLabel("Confirm Password").type("password");
   await page.getByRole("button", { name: "Register" }).click();
   await expect(
     page.getByText("This e-mail address is already registered")
-  ).toBeVisible();
-});
-
-test("User cannot register with mismatched passwords", async ({ page }) => {
-  await page.getByLabel("Username").type("ashoka1");
-  await page.getByLabel("E-mail").type("ashoka@email.com");
-  await page.getByLabel("Password:", { exact: true }).type("password");
-  await page.getByLabel("Confirm Password").type("password1");
-  await page.getByRole("button", { name: "Register" }).click();
-  await expect(
-    page.getByText("Password and confirm password must match")
   ).toBeVisible();
 });
